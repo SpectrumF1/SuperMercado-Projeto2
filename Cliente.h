cc37b7df42f9b3a494d5db9a7afe6d4c1abb1b2e
ineeve <up201504942@FEUPSIG.FE.UP.PT>
2016-05-09 09:57:53 +0100

    Edicao da classe clientes
    Implementacao de sets e gets na classe clientes

--- Cliente.h
+++ Cliente.h
@@ -13,16 +13,20 @@ class Cliente {
  private:
   unsigned int id;
   string nome;
-  Data cartaoCliente;
+  Data dataAdesao;
   float volCompras;
 
  public:
   Cliente(ifstream & in);
   string getNome() const;
   unsigned int getId() const;
+  Data getDataAdesao() const;
   float getVolCompras() const;
   void save(ofstream & out) const;
   friend ostream& operator<<(ostream& out, const Cliente & cli);  // mostra informacao do cliente no ecra
   friend bool operator<(const Cliente &cli1, const Cliente &cli2); // compara 2 clientes (1 cleinte e menor que outro se o seu nome for "alfabeticamente" inferior)
+  void setNome(string newName);
+  void setDataAdesao(Data newDate);
+  void setVolCompras(float newVolCompras);
 };
 
