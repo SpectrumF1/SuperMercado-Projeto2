e9f686c27b7c0a9bc83d59606577583dd079c03e
Renato Alexandre Sousa Campos <renatoineeve@gmail.com>
2016-05-19 18:36:10 +0100

    Comment Debug Zone

--- Vende++.cpp
+++ Vende++.cpp
@@ -467,7 +467,7 @@ string VendeMaisMais::matrizRecomendacao(unsigned int clienteId) {
 	vector <MatrizHitVec> analysis;
 	vector <int> maxidVEC;
 
-	/*DEBUGGING ZONE*/
+	/*DEBUGGING ZONE
 	//Header (produtos iniciais)
 	cout << "ID";
 	for (unsigned int produtosIndex = 0; produtosIndex < produtosVector.size(); produtosIndex++) {
